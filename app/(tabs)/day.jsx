cf91e4449795d3733cbb6afd29cf42b39a039dff
yellowsubmarine372 <yellowsubmarine372@gmail.com>
2025-07-12 22:44:28 +0900

    refactor layout and added tabs

---
+++ app/(tabs)/day.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function DayScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Day View</Text>
+    </View>
+  )
+}
---
+++ app/(tabs)/week.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function WeekScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Week View</Text>
+    </View>
+  )
+}
